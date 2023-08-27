fb1df4b04f8a9bdec8567991da4c1f269fb0b42d
leonardo valle <leo_mvalle@outlook.com>
2023-08-27 14:56:51 -0300

    exibe card de produtos na pagina de produtos

---
+++ pages/products/products.js
@@ -0,0 +1,37 @@
+const getProducts = async () => {
+  try {
+    const response = await fetch("http://localhost:3002/products");
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    alert("erro ao realizar a requisição", e.message);
+  }
+};
+
+const createCard = function (product) {
+  return ` <div class="card">
+  <figure class="card-img-container">
+    <img
+      class="card-img"
+      src="../../assets/${product.imgId}.svg"
+      alt=${product.name}
+    />
+  </figure>
+  <div class="card-info">
+    <span>${product.name}</span>
+    <span>preço: R$ ${product.price}</span>
+    <button class="card-btn-add-cart">Adicionar ao carrinho</button>
+  </div>
+</div>`;
+};
+
+const getCard = async () => {
+  const { products } = await getProducts();
+  const cardContainer = document.querySelector(".card-container");
+
+  for (let product of products) {
+    cardContainer.innerHTML += createCard(product);
+  }
+};
+
+getCard();
